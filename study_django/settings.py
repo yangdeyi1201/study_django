36b58e6465afc7fec861d34df4dd173bee13421c
yangdeyi <yangdeyi1201@foxmail.com>
2022-07-13 18:38:58 +0800

    django配置数据库连接

--- study_django/settings.py
+++ study_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'students.apps.StudentsConfig'
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,21 @@ WSGI_APPLICATION = 'study_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'study_orm',  # 数据库名
+        'USER': 'root',
+        'PASSWORD': 'yey1201abc',
+        'HOST': '114.116.114.106',
+        'PORT': '4000'
     }
 }
 
